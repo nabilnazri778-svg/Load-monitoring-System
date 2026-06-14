560a7b9ecbd9845f259431c99c596a61f73df7ac
nabilnazri778-svg <nabilnazri778@gmail.com>
2026-06-15 01:17:11 +0800

    Update 4_About.py

---
+++ pages/4_About.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.title("ℹ️ About")
+
+st.write("""
+IoT Structural Learning & Load Monitoring System
+
+Hardware:
+- ESP32
+- HX711
+- Load Cell
+
+Features:
+- Force Monitoring
+- Learning Notes
+- Quiz Module
+""")
